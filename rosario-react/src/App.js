606b20e0d667b46ae8746d4a23ce1c521dec958f
SmgcGithub07 <saulo0707@hotmail.com>
2022-12-30 13:25:57 -0600

    Se agregó un menú temporal tipo dropdown y se empieza con el insert de enfermedades

--- rosario-react/src/App.js
+++ rosario-react/src/App.js
@@ -1,22 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
-import  Listarenfermedades  from "./componentes/Listar-enfermedades";
+import  Listarenfermedades  from "./componentes/Catalogo-Enfermedades/Listar-enfermedades";
 import { Route, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
    <Router>
-     <div className="container">
-          <nav class="navbar navbar-expand navbar-light bg-light">
-              <div class="nav navbar-nav">
-                  <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-                  <a class="nav-item nav-link" href="#">Home</a>
-              </div>
-          </nav>
-
-          <Listarenfermedades></Listarenfermedades>
-          
-      </div>
+         <div className='container'>
+            <ul class="nav nav-tabs">
+              <li class="nav-item">
+                <a href="#" class="nav-link active">Active</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#">Action</a>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">Another link</a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link disabled">Disabled</a>
+              </li>
+            </ul>
+                <Listarenfermedades></Listarenfermedades>
+         </div>       
     </Router>
   );
 }
